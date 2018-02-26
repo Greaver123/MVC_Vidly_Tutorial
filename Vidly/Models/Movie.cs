6ca4d0e329a49dfad621933c6ce50dbd0d49742d
Greaver123 <p.kolodziej92@gmail.com>
2018-02-26 21:41:03 +0100

    Added Date annotations to Movie

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,7 +11,10 @@ namespace Vidly.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public Genre Genre { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [DataType(DataType.Date)]
         public DateTime DateAdded { get; set; }
         public long NumberInStock { get; set; }
     }
